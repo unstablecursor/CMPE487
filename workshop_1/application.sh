468c903d9b739e626b39c1958897bdaf5ee1d08d
Recep Deniz Aksoy <recepdeniza@gmail.com>
2018-10-14 21:21:27 +0300

    Stable version 0.1

--- workshop_1/application.sh
+++ workshop_1/application.sh
@@ -11,11 +11,6 @@ discovery(){
     done 
 }
 
-delete_duplicates(){
-    var=$(awk '!a[$0]++' client_list.txt)
-    echo "$var" > client_list.txt
-}
-
 be_discoverable(){
     sender_name=$1
     while true;do
@@ -41,17 +36,10 @@ be_discoverable(){
     done
 }
 
-decrypt_msg(){
-    second_c=$(echo "$1" | md5)
-    echo $second_c
-    if [ "$second_c" == "$2" ]
-    then
-        echo "1"
-    else
-        echo "0"
-    fi
+delete_duplicates(){
+    var=$(awk '!a[$0]++' client_list.txt)
+    echo "$var" > client_list.txt
 }
-
 
 recieve_message(){
     while true;do
@@ -59,34 +47,15 @@ recieve_message(){
         request=$(nc -l 5001)
         local sender_ip
         sender_ip=$(echo "$request" | cut -d';' -f1)
-        local cyp
-        cyp=$(echo "$request" | cut -d';' -f2)
+        touch "$sender_ip.txt"
+        local cyper
+        cyper=$(echo "$request" | cut -d';' -f2)
         local message
         message=$(echo "$request" | cut -d';' -f3)
         echo "$message" >> "$sender_ip.txt"
-            
-        # if [ "$client_ip" == "$sender_ip" ]; then
-        #     cypher=$cyp
-        # fi 
-        # if [ -e "./$sender_ip.txt" ]; then
-        #     file_cyp=$(head -n 1 "$sender_ip.txt")
-        #     local res
-        #     res=$(decrypt_msg "$file_cyp" "$cyp")
-        #     echo $res
-        #     if [ "$res" = "1" ]
-        #     then
-        #         sed -i '' "1s/.*/$cyp/" "$sender_ip.txt"
-                
-        #     fi
-        # else 
-        #     touch "$sender_ip.txt"
-        #     echo "$cyp" >> "$sender_ip.txt"
-        # fi 
     done
 }
 
-#TODO: Randomly generate cypher
-cypher="sdkjhaskjhd" 
 pkill -f "tail -f"
 pkill -f "nc -l"
 my_ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
@@ -109,22 +78,14 @@ while true; do
     client_ip=$(echo "$client_info" | cut -d';' -f1)
     echo $client_ip   
     echo "To exit the room, type SIGEXIT"
-    touch "$client_ip.txt"
-    # if [ -e "$client_ip.txt" ]; then
-    #     cypher=$(head -n 1 "$client_ip.txt")
-    # else 
-    #     touch "$client_ip.txt"
-    #     echo "$cypher" >> "$client_ip.txt"
-    # fi
+    touch "$client_ip.txt"  
     pkill -f "tail -f"
     tail -f "$client_ip.txt" &
-    while true; do    
-        #cypher=$(head -n 1 "$client_ip.txt") 
+    while true; do     
         read msg
         if [ "$msg" = "SIGEXIT" ]; then
             break
         fi
-        # new_c=$(echo "$cypher" | md5)
         echo "$my_ip;1;$msg" | nc -G 1 "$client_ip" 5001 &
     done
 done
